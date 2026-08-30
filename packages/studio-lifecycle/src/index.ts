5196335f1c9c9edadb162e555d3c81a03f382e5d
hrpering <128376948+hrpering@users.noreply.github.com>
2026-08-30 06:03:29 +0300

    feat(studio): export lifecycle service

---
+++ packages/studio-lifecycle/src/index.ts
@@ -0,0 +1,18 @@
+export { createStudioLifecycleService } from "./service.js";
+export { STUDIO_LIFECYCLE_RECORD_VERSION } from "./types.js";
+export type {
+  StudioLifecycleCreateInput,
+  StudioLifecycleIssue,
+  StudioLifecycleIssueCode,
+  StudioLifecycleRecord,
+  StudioLifecycleResult,
+  StudioLifecycleSaveInput,
+  StudioLifecycleService,
+  StudioLifecycleServiceConfiguration,
+  StudioLifecycleStore,
+  StudioLifecycleStoreDeleteResult,
+  StudioLifecycleStoreMutationCode,
+  StudioLifecycleStoreMutationResult,
+  StudioLifecycleSummary,
+  StudioLifecycleVersionedInput,
+} from "./types.js";
