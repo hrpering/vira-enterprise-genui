8fe85f846a20060fa17a68a34ced4b56a42e0ff4
hrpering <128376948+hrpering@users.noreply.github.com>
2026-08-31 21:19:43 +0300

    GENUI-128: expose GenUI web component API

---
+++ packages/genui-web-component/src/index.ts
@@ -0,0 +1,11 @@
+export {
+  VIRA_GENUI_EXPERIENCE_TAG_NAME,
+  createViraGenUIElementClass,
+  defineViraGenUIElement,
+} from "./element.js";
+export type {
+  ViraGenUIElementApi,
+  ViraGenUIElementConstructor,
+  ViraGenUIElementMountInput,
+  ViraGenUIElementMountResult,
+} from "./element.js";
