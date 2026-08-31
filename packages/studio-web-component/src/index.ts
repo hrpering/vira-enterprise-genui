7f58f30761391db33ea83efba429f5ecddab6023
hrpering <128376948+hrpering@users.noreply.github.com>
2026-08-31 20:48:38 +0300

    GENUI-131: export Studio custom element bridge

---
+++ packages/studio-web-component/src/index.ts
@@ -0,0 +1,17 @@
+export {
+  createStudioExperienceElementClass,
+  defineStudioExperienceElement,
+} from "./element.js";
+export { VIRA_STUDIO_EXPERIENCE_TAG_NAME } from "./types.js";
+export type {
+  StudioExperienceElementApi,
+  StudioExperienceElementConfiguration,
+  StudioExperienceElementConstructor,
+  StudioExperienceElementDefineResult,
+  StudioExperienceElementIssue,
+  StudioExperienceElementPlatform,
+  StudioExperienceElementReactRoot,
+  StudioExperienceElementReactRootFactory,
+  StudioExperienceElementResult,
+  StudioExperienceElementValidationCode,
+} from "./types.js";
