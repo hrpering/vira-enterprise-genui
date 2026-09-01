564a2223e03cba87eeab72ab6810ce2c064dba09
hrpering <128376948+hrpering@users.noreply.github.com>
2026-09-01 07:35:42 +0300

    REG-001: expose Experience Registry API

---
+++ packages/experience-registry/src/index.ts
@@ -0,0 +1,24 @@
+export {
+  createExperienceRegistrySnapshot,
+  lookupExperienceRegistryManifest,
+} from "./registry.js";
+export {
+  EXPERIENCE_REGISTRY_MAX_ARRAY_LENGTH,
+  EXPERIENCE_REGISTRY_MAX_DEPTH,
+  EXPERIENCE_REGISTRY_MAX_MANIFESTS,
+  EXPERIENCE_REGISTRY_MAX_NODES,
+  EXPERIENCE_REGISTRY_QUERY_ID_MAX_LENGTH,
+  EXPERIENCE_REGISTRY_QUERY_VERSION_MAX_LENGTH,
+  EXPERIENCE_REGISTRY_SCHEMA_VERSION,
+} from "./types.js";
+export type {
+  ExperienceRegistryLookup,
+  ExperienceRegistryLookupCode,
+  ExperienceRegistryLookupIssue,
+  ExperienceRegistryLookupQuery,
+  ExperienceRegistryLookupResult,
+  ExperienceRegistrySnapshot,
+  ExperienceRegistrySnapshotResult,
+  ExperienceRegistryValidationCode,
+  ExperienceRegistryValidationIssue,
+} from "./types.js";
