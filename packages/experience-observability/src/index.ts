225d067755867621eac158f686231b59853391f5
hrpering <128376948+hrpering@users.noreply.github.com>
2026-09-01 05:31:13 +0300

    EOBS-001: export observability API

---
+++ packages/experience-observability/src/index.ts
@@ -0,0 +1,12 @@
+export { createExperienceObservation } from "./observation.js";
+export { EXPERIENCE_OBSERVATION_NAMES } from "./types.js";
+export type {
+  ExperienceObservationDefinition,
+  ExperienceObservationDurationPolicy,
+  ExperienceObservationInput,
+  ExperienceObservationIssue,
+  ExperienceObservationName,
+  ExperienceObservationResult,
+  ExperienceObservationValidationCode,
+  ExperienceObservationValidationIssue,
+} from "./types.js";
