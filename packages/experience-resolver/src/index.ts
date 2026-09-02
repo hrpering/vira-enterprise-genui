4589d3f0d52a5d41c0ffd8b2d7709fcb0cd7acff
hrpering <128376948+hrpering@users.noreply.github.com>
2026-09-03 00:57:42 +0300

    MASTER-05: export experience resolver contract

---
+++ packages/experience-resolver/src/index.ts
@@ -0,0 +1,23 @@
+export {
+  createExperienceResolver,
+  EXPERIENCE_RESOLUTION_REQUEST_VERSION,
+  EXPERIENCE_RESOLVER_MAX_ID_LENGTH,
+} from "./resolver.js";
+export type {
+  ExperienceArtifactResolutionContext,
+  ExperienceExactDeploymentTarget,
+  ExperienceHostRequirementContext,
+  ExperienceResolutionArtifactIdentity,
+  ExperienceResolutionCode,
+  ExperienceResolutionCompatibilityIdentity,
+  ExperienceResolutionIssue,
+  ExperienceResolutionPackIdentity,
+  ExperienceResolutionRequest,
+  ExperienceResolutionResult,
+  ExperienceResolver,
+  ExperienceResolverConfiguration,
+  ExperienceResolverConfigurationCode,
+  ExperienceResolverConfigurationIssue,
+  ExperienceResolverFactoryResult,
+  ResolvedExperienceDescriptor,
+} from "./resolver.js";
