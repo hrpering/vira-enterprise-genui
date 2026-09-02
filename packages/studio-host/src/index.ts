e4d5ff1e6e73b2c0eda122ee6164d7abafad9502
hrpering <128376948+hrpering@users.noreply.github.com>
2026-09-03 00:37:42 +0300

    MASTER-04: export host capability contracts

--- packages/studio-host/src/index.ts
+++ packages/studio-host/src/index.ts
@@ -4,6 +4,31 @@ export {
   createStudioHostSnapshot,
 } from "./validate.js";
 export {
+  createStudioHostCapabilityManifest,
+  createStudioHostCompatibilityRequirement,
+  evaluateStudioHostCompatibility,
+  STUDIO_HOST_CAPABILITY_MANIFEST_VERSION,
+  STUDIO_HOST_COMPATIBILITY_REQUIREMENT_VERSION,
+  STUDIO_HOST_MAX_CAPABILITIES,
+  STUDIO_HOST_MAX_IMPLEMENTATION_IDS,
+  STUDIO_HOST_PLATFORMS,
+} from "./capability-manifest.js";
+export type {
+  StudioHostCapabilityManifest,
+  StudioHostCapabilityManifestResult,
+  StudioHostCapabilityValidationCode,
+  StudioHostCapabilityValidationIssue,
+  StudioHostCompatibilityEvaluation,
+  StudioHostCompatibilityEvaluationResult,
+  StudioHostCompatibilityInputIssue,
+  StudioHostCompatibilityInputStage,
+  StudioHostCompatibilityMismatch,
+  StudioHostCompatibilityMismatchCode,
+  StudioHostCompatibilityRequirement,
+  StudioHostCompatibilityRequirementResult,
+  StudioHostPlatform,
+} from "./capability-manifest.js";
+export {
   STUDIO_HOST_ACTION_OUTCOMES,
   STUDIO_HOST_BRIDGE_VERSION,
   STUDIO_HOST_SNAPSHOT_VERSION,
