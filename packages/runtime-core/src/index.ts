31fe3bad6a72f65a0492c28ea13755bf67a07784
hrpering <128376948+hrpering@users.noreply.github.com>
2026-09-03 09:04:22 +0300

    MASTER-06: expose runtime session kernel

--- packages/runtime-core/src/index.ts
+++ packages/runtime-core/src/index.ts
@@ -2,6 +2,7 @@ export * from "./state/index.js";
 export * from "./actions/index.js";
 export * from "./patches/index.js";
 export * from "./lifecycle/index.js";
+export * from "./session/index.js";
 export * from "./permissions/index.js";
 export * from "./errors/index.js";
 export * from "./reducer/index.js";
