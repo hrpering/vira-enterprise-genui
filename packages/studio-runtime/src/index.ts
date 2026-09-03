3fdd8bfa0358556b837a31f8a65c50c2e9f9a83a
hrpering <128376948+hrpering@users.noreply.github.com>
2026-09-03 14:28:40 +0300

    MASTER-07B: export runtime expansion budget

--- packages/studio-runtime/src/index.ts
+++ packages/studio-runtime/src/index.ts
@@ -1,3 +1,3 @@
 export { createStudioRuntimeSession } from "./session.js";
-export { STUDIO_RUNTIME_MAX_REPEAT_ITEMS } from "./types.js";
+export { STUDIO_RUNTIME_MAX_EXPANDED_NODES, STUDIO_RUNTIME_MAX_REPEAT_ITEMS } from "./types.js";
 export type { CreateStudioRuntimeSessionResult, StudioRuntimeCompletion, StudioRuntimeCompletionResult, StudioRuntimeDataPort, StudioRuntimeDispatchResult, StudioRuntimeIssue, StudioRuntimeNodeModel, StudioRuntimePorts, StudioRuntimeSession, StudioRuntimeValidationCode, StudioRuntimeViewModel, StudioRuntimeViewResult } from "./types.js";
