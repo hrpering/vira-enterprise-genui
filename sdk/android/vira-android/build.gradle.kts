8c7d784a6e409a3bd80e55a6437a7eca2e43b09b
hrpering <128376948+hrpering@users.noreply.github.com>
2026-09-03 16:41:09 +0300

    build(android): add Vira Android library module

---
+++ sdk/android/vira-android/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+  id("com.android.library")
+}
+
+android {
+  namespace = "xyz.tryvira.android"
+  compileSdk = 37
+
+  defaultConfig {
+    minSdk = 26
+    consumerProguardFiles("consumer-rules.pro")
+  }
+
+  sourceSets {
+    getByName("main").java.srcDir("../../../interop/studio-experience/v1/kotlin")
+  }
+
+  testOptions {
+    unitTests.isIncludeAndroidResources = false
+  }
+}
+
+dependencies {
+  testImplementation("junit:junit:4.13.2")
+}
