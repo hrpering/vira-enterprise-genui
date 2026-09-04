5f8ac6d01574ce2d3e6e60ca08bc68ceb5ae8734
hrpering <128376948+hrpering@users.noreply.github.com>
2026-09-04 18:58:57 +0300

    feat(master-36): export Canvas design import surface

---
+++ packages/application-canvas-design-import/src/index.ts
@@ -0,0 +1,16 @@
+export { importViraCanvasDesignSystem } from "./import.js";
+export {
+  VIRA_CANVAS_DESIGN_IMPORT_MAX_REVISION_LENGTH,
+  VIRA_CANVAS_DESIGN_IMPORT_MAX_SOURCE_ID_LENGTH,
+  VIRA_CANVAS_DESIGN_IMPORT_MODE,
+  VIRA_CANVAS_DESIGN_IMPORT_VERSION,
+  VIRA_CANVAS_DESIGN_SOURCE_FORMAT,
+} from "./types.js";
+export type {
+  ViraCanvasBoundBrandReference,
+  ViraCanvasDesignImportArtifact,
+  ViraCanvasDesignImportIssue,
+  ViraCanvasDesignImportIssueCode,
+  ViraCanvasDesignImportResult,
+  ViraCanvasExternalDesignSource,
+} from "./types.js";
