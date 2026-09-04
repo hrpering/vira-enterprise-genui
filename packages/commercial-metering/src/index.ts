8182f5e4361f4b2ab48d1e52482e552c8488d843
hrpering <128376948+hrpering@users.noreply.github.com>
2026-09-05 00:13:23 +0300

    feat(master-43): export commercial metering API

---
+++ packages/commercial-metering/src/index.ts
@@ -0,0 +1,32 @@
+export {
+  VIRA_COMMERCIAL_METERING_MAX_METERS,
+  VIRA_COMMERCIAL_METERING_MAX_USAGE_RECORDS,
+  VIRA_COMMERCIAL_METERING_SCHEMA_VERSION,
+  VIRA_COMMERCIAL_METER_UNITS,
+  VIRA_COMMERCIAL_METER_WINDOWS,
+  VIRA_COMMERCIAL_USAGE_RATING_STATUSES,
+} from "./types.js";
+export type {
+  ViraCommercialMeterCatalog,
+  ViraCommercialMeterCatalogResult,
+  ViraCommercialMeterDefinition,
+  ViraCommercialMeteringIssue,
+  ViraCommercialMeteringIssueCode,
+  ViraCommercialMeteringSerializationResult,
+  ViraCommercialMeterUnit,
+  ViraCommercialMeterWindow,
+  ViraCommercialUsageBatch,
+  ViraCommercialUsageBatchResult,
+  ViraCommercialUsageRating,
+  ViraCommercialUsageRatingRequest,
+  ViraCommercialUsageRatingResult,
+  ViraCommercialUsageRatingStatus,
+  ViraCommercialUsageRecord,
+} from "./types.js";
+export {
+  parseViraCommercialMeterCatalog,
+  parseViraCommercialUsageBatch,
+  rateViraCommercialUsage,
+  serializeViraCommercialMeterCatalog,
+  serializeViraCommercialUsageBatch,
+} from "./metering.js";
